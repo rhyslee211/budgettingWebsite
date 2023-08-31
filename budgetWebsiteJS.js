32fa60ecd95be9b57c0299d12b12d4cebcdeb475
rhyslee211 <rhyslee211@gmail.com>
2023-08-31 09:25:19 -0400

    Fixed loss of input data when adding items to debts or expenses

--- budgetWebsiteJS.js
+++ budgetWebsiteJS.js
@@ -1,12 +1,18 @@
 function addDebt(){
 
-    document.getElementById('debtInputDiv').innerHTML += "<li><input class='debtNameInput' placeholder='Type of Debt'> <input class = 'debtAmtInput' placeholder='Debt Amount'></li>";
+    const  debtElem = document.createElement('li')
+    debtElem.innerHTML = "<input class='debtNameInput' placeholder='Type of Expense'> <input class = 'debtAmtInput' placeholder='Expense Amount'>"
+
+    document.getElementById('debtInputList').appendChild(debtElem)
 
 }
 
 function addExpense(){
 
-    document.getElementById('expensesInputDiv').innerHTML += "<li><input class='expenseNameInput' placeholder='Type of Expense'> <input class = 'expenseAmtInput' placeholder='Expense Amount'></li>";
+    const  expenseElem = document.createElement('li')
+    expenseElem.innerHTML = "<input class='expenseNameInput' placeholder='Type of Expense'> <input class = 'expenseAmtInput' placeholder='Expense Amount'>"
+
+    document.getElementById('expenseInputList').appendChild(expenseElem)
 
 }
 
@@ -21,9 +27,14 @@ function calcBudget(){
     //console.log(debtCollection)
     //console.log(expenseCollection)
 
+    if(isNaN(parseFloat(document.getElementById('monthlyPay').value))){
+        alert("Please enter your monthly pay")
+        return
+    }
+
     for (let i = 0; i < debtCollection.length; i++){
 
-        if(!isNaN(debtCollection[i].value)){
+        if(!isNaN(parseFloat(debtCollection[i].value))){
 
             //console.log(debtCollection[i].value)
             budgetNum -= parseFloat(debtCollection[i].value)
@@ -32,13 +43,23 @@ function calcBudget(){
 
     for (let i = 0; i < expenseCollection.length; i++){
 
-        if(!isNaN(expenseCollection[i].value)){
+        if(!isNaN(parseFloat(expenseCollection[i].value))){
 
             //console.log(expenseCollection[i].value)
             budgetNum -= parseFloat(expenseCollection[i].value)
         }
     } 
 
-    document.getElementById('displayBudget').innerHTML = 'Your leftover money is: ' + budgetNum
+    console.log(budgetNum)
+
+    if(budgetNum > 0){
+        document.getElementById('displayBudget').innerHTML = 'Your leftover money is: ' + budgetNum
+    }
+    else if(budgetNum == 0){
+        document.getElementById('displayBudget').innerHTML = 'Your budget is perfectly balanced.'
+    }
+    else{
+        document.getElementById('displayBudget').innerHTML = 'Your over your budget by ' + Math.abs(budgetNum)
+    }
 
 }
