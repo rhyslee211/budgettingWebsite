6422d3e9d02ed5154395cc9845c9db9c1f914409
Rhys Lee <78373339+rhyslee211@users.noreply.github.com>
2023-09-02 04:06:39 -0400

    added categories for debt and expense types

--- budgetWebsiteJS.js
+++ budgetWebsiteJS.js
@@ -1,7 +1,15 @@
 function addDebt(){
 
     const  debtElem = document.createElement('li')
-    debtElem.innerHTML = "<input class='debtNameInput' placeholder='Type of Expense'> <input class = 'debtAmtInput' placeholder='Expense Amount'>"
+    debtElem.innerHTML = "<select>\
+            <option value = ''>-----Type of Debt-----</option>\
+            <option value = 'Home'>Home</option>\
+            <option value = 'Auto'>Auto</option>\
+            <option value = 'Student Loans'>Student Loans</option>\
+            <option value = 'Credit Card'>Credit Card</option>\
+            <option value = 'Other'>Other</option>\
+        </select> <input class='debtNameInput' placeholder='Type of Expense'> \
+        <input class = 'debtAmtInput' placeholder='Expense Amount'>"
 
     document.getElementById('debtInputList').appendChild(debtElem)
 
@@ -10,7 +18,17 @@ function addDebt(){
 function addExpense(){
 
     const  expenseElem = document.createElement('li')
-    expenseElem.innerHTML = "<input class='expenseNameInput' placeholder='Type of Expense'> <input class = 'expenseAmtInput' placeholder='Expense Amount'>"
+    expenseElem.innerHTML = "<select>\
+                <option value = ''>--Type of Expense--</option>\
+                <option value = 'Bills'>Bills</option>\
+                <option value = 'Groceries'>Groceries</option>\
+                <option value = 'Clothing'>Clothing</option>\
+                <option value = 'Transportation'>Transportation</option>\
+                <option value = 'Insurance'>Insurance</option>\
+                <option value = 'Savings'>Savings</option>\
+            </select> \
+            <input class='expenseNameInput' placeholder='Type of Expense'> \
+            <input class = 'expenseAmtInput' placeholder='Expense Amount'>"
 
     document.getElementById('expenseInputList').appendChild(expenseElem)
 
@@ -22,7 +40,9 @@ function calcBudget(){
 
     const expenseCollection = document.getElementsByClassName('expenseAmtInput')
 
-    var budgetNum = parseFloat(document.getElementById('monthlyPay').value)
+    var incomeNum = parseFloat(document.getElementById('monthlyPay').value)
+
+    var budgetNum = incomeNum
 
     //console.log(debtCollection)
     //console.log(expenseCollection)
@@ -52,14 +72,17 @@ function calcBudget(){
 
     console.log(budgetNum)
 
+
     if(budgetNum > 0){
-        document.getElementById('displayBudget').innerHTML = 'Your leftover money is: ' + budgetNum
+        document.getElementById('displayBudget').innerHTML = '<div>Your leftover money is: ' + budgetNum + '</div>'
     }
     else if(budgetNum == 0){
-        document.getElementById('displayBudget').innerHTML = 'Your budget is perfectly balanced.'
+        document.getElementById('displayBudget').innerHTML = '<div>Your budget is perfectly balanced.' + '</div>'
     }
     else{
-        document.getElementById('displayBudget').innerHTML = 'Your over your budget by ' + Math.abs(budgetNum)
+        document.getElementById('displayBudget').innerHTML = '<div>Your over your budget by ' + Math.abs(budgetNum) + '</div>'
     }
+
+    
 
 }
